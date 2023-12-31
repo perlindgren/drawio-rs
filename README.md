36606a75dd8fdc7f84544530b046bbcc1d88cfab
Per Lindgren <per.lindgren@ltu.se>
2023-12-31 14:13:24 +0100

    wip

---
+++ README.md
@@ -0,0 +1,20 @@
+# Drawio-rs
+
+Crate for drawing graphics using `Draw.io`.
+
+## Installation
+
+First install `Drawio.io` locally on your computer and make it available from terminal.
+
+### Native Windows
+
+- Install the program [draw.io](https://www.drawio.com/).
+- Add to path for the shell you use, e.g., in PowerShell `$Env:Path += ';<PATH>`, where `<PATH>` is the full path to the folder where the `draw.io.exe` is stored - in the case of a system wide install:
+  
+  ```shell
+  $Env:Path += ';C:\Program Files\draw.io'
+  ```
+
+### Linux, Osx and Windows WSL
+
+- Install `draw.io` using your package manager, should be in path by default.
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// example header
+
+// <mxfile host="Electron" modified="2023-12-31T09:23:31.582Z" agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) draw.io/22.1.16 Chrome/120.0.6099.109 Electron/28.1.0 Safari/537.36" etag="1o7Z_KPdkwODHp7ASC18" version="22.1.16" type="device">
+//   <diagram name="Page-1" id="Gpl6vU_g16wYDfX3Skah">
+//     <mxGraphModel dx="1098" dy="988" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="827" pageHeight="1169" math="0" shadow="0">
+//       <root>
+//         <mxCell id="0" />
+//         <mxCell id="1" parent="0" />
+//         <mxCell id="pQSwlj2l13oX5aKklSoz-1" value="" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" parent="1">
+//           <mxGeometry x="160" y="320" width="40" height="80" as="geometry" />
+//         </mxCell>
+//       </root>
+//     </mxGraphModel>
+//   </diagram>
+// </mxfile>
+
+pub struct DrawIo {}
